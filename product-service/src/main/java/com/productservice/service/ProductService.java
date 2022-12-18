eaf103b79baa4de8132771afa36e0c08768f8e5d
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-18 14:03:17 +0700

    add service method getAllProducts

--- product-service/src/main/java/com/productservice/service/ProductService.java
+++ product-service/src/main/java/com/productservice/service/ProductService.java
@@ -1,10 +1,14 @@
 package com.productservice.service;
 
 import com.productservice.dto.ProductRequest;
+import com.productservice.dto.ProductResponse;
 import com.productservice.model.Product;
 import com.productservice.repository.ProductRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
@@ -27,4 +31,22 @@ public class ProductService {
         log.info("Product {} is saved", product.getId());
 
     }
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+
+        return products.stream()
+                .map(this::mapToProductResponse)
+                .collect(Collectors.toList());
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
+                .build();
+    }
+
 }
