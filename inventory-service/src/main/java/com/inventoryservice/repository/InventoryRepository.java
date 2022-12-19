75f3994833dcb5a0f798e9f7f53cff8da59da1b7
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-19 14:42:24 +0700

    add repository InventoryRepository

---
+++ inventory-service/src/main/java/com/inventoryservice/repository/InventoryRepository.java
@@ -0,0 +1,10 @@
+package com.inventoryservice.repository;
+
+import com.inventoryservice.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InventoryRepository extends JpaRepository<Inventory, String> {
+
+}
