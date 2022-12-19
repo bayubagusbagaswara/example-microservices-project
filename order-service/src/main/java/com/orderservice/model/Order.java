98565f56c3346a40e29453de87f94cabf6b47731
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-19 09:01:03 +0700

    add model Order

---
+++ order-service/src/main/java/com/orderservice/model/Order.java
@@ -0,0 +1,30 @@
+package com.orderservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    @Id
+    @GenericGenerator(name = "uuid-gen", strategy = "uuid2")
+    @GeneratedValue(generator = "uuid-gen")
+    private String id;
+
+    private String orderNumber;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<OrderLineItems> orderLineItemsList;
+
+}
