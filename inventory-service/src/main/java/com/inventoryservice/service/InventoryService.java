9aa4362cf33b6dbfd34357697aaf77207dfbde0e
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-19 14:42:11 +0700

    update service InventoryService

---
+++ inventory-service/src/main/java/com/inventoryservice/service/InventoryService.java
@@ -0,0 +1,14 @@
+package com.inventoryservice.service;
+
+import com.inventoryservice.repository.InventoryRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+
+    public InventoryService(InventoryRepository inventoryRepository) {
+        this.inventoryRepository = inventoryRepository;
+    }
+}
