94725046b23e71ed079c2e50b29f8a06ceb77e8c
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-24 16:51:23 +0700

    add method isInStock

--- inventory-service/src/main/java/com/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/inventoryservice/service/InventoryService.java
@@ -2,6 +2,7 @@ package com.inventoryservice.service;
 
 import com.inventoryservice.repository.InventoryRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class InventoryService {
@@ -11,4 +12,10 @@ public class InventoryService {
     public InventoryService(InventoryRepository inventoryRepository) {
         this.inventoryRepository = inventoryRepository;
     }
+
+    @Transactional(readOnly = true)
+    public boolean isInStock(String skuCode) {
+        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    }
+
 }
