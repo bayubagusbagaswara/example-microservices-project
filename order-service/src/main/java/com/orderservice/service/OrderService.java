3b0d237ee9151bb68c42ac441c9c5a71a51c48b5
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-24 16:50:46 +0700

    add annotation Transactional

--- order-service/src/main/java/com/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/orderservice/service/OrderService.java
@@ -6,12 +6,14 @@ import com.orderservice.model.Order;
 import com.orderservice.model.OrderLineItems;
 import com.orderservice.repository.OrderRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
+@Transactional
 public class OrderService {
 
     private final OrderRepository orderRepository;
