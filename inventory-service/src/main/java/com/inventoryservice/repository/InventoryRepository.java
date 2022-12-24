f2077c6633f0e23d2046132725edf2a179b5bdac
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-24 16:51:11 +0700

    add method findBySkuCode

--- inventory-service/src/main/java/com/inventoryservice/repository/InventoryRepository.java
+++ inventory-service/src/main/java/com/inventoryservice/repository/InventoryRepository.java
@@ -4,7 +4,10 @@ import com.inventoryservice.model.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface InventoryRepository extends JpaRepository<Inventory, String> {
 
+    Optional<Inventory> findBySkuCode(String skuCode);
 }
