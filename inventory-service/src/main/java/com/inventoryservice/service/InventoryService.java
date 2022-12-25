c9d17cdea1c40e9ad2e3b736843b789d0624b3f4
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-25 17:03:05 +0700

    update method isInStock

--- inventory-service/src/main/java/com/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/inventoryservice/service/InventoryService.java
@@ -1,8 +1,12 @@
 package com.inventoryservice.service;
 
+import com.inventoryservice.dto.InventoryResponse;
 import com.inventoryservice.repository.InventoryRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class InventoryService {
@@ -14,8 +18,12 @@ public class InventoryService {
     }
 
     @Transactional(readOnly = true)
-    public boolean isInStock(String skuCode) {
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    public List<InventoryResponse> isInStock(List<String> skuCode) {
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+                .map(inventory -> InventoryResponse.builder().skuCode(inventory.getSkuCode())
+                        .isInStock(inventory.getQuantity() > 0)
+                        .build())
+                .collect(Collectors.toList());
     }
 
 }
