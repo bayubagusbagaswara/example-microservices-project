207388ba2a7454944d3d505f79789016b6f9bc08
bayubagusbagaswara <bagaszwara12@gmail.com>
2022-12-25 17:03:12 +0700

    update method isInStock

--- inventory-service/src/main/java/com/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/inventoryservice/controller/InventoryController.java
@@ -1,8 +1,11 @@
 package com.inventoryservice.controller;
 
+import com.inventoryservice.dto.InventoryResponse;
 import com.inventoryservice.service.InventoryService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/inventory")
@@ -16,7 +19,8 @@ public class InventoryController {
 
     @GetMapping("/{sku-code}")
     @ResponseStatus(HttpStatus.OK)
-    public boolean isInStock(@PathVariable("sku-code") String skuCode) {
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
+
         return inventoryService.isInStock(skuCode);
     }
 
